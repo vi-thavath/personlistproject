7d38d904e34588d77894c56396700a36c0ace5b4
vi-thavath <vithavath.yeng@gmail.com>
2022-06-29 16:39:07 +0700

    okay

--- src/App.js
+++ src/App.js
@@ -1,55 +1,144 @@
-import React from "react";
-// import "./App.css";
+import React, { Component } from "react";
 import "./App.css";
+import Proptypes from "prop-types";
 
-const App = () => <PersonList />;
-
-const PersonList = () => {
-  const people = [
-    {
-      img: 22,
-      job: "developer",
-      name: "john",
-    },
-    {
-      img: 32,
-      job: "designer",
-      name: "jose",
-    },
-    {
-      img: 52,
-      job: "engineer",
-      name: "Jean",
-    },
-  ];
+const Person = ({ img, name, age, info }) => {
   return (
-    <section>
-      <Person person={people[0]} />
-      <Person person={people[1]}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione fugiat
-        dolorem aut similique laborum.
-      </Person>
-
-      <Person person={people[2]} />
-    </section>
-  );
-};
-//gg this week
-const Person = (props) => {
-  const { img, job, name } = props.person;
-  const { children } = props;
-  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
-
-  return (
-    <div className="person">
-      <img src={url} alt="" />
-      <div>
-        <h4>{name}</h4>
-        <h4>{job}</h4>
-        {children}
-      </div>
+    <div>
+      <img src={img} alt="person" />
+      <h2>name: {name}</h2>
+      <h2>age: {age}</h2>
+      <h4>info:{info}</h4>
     </div>
   );
 };
+
+Person.propTypes = {
+  img: Proptypes.string.isRequired,
+  name: Proptypes.string.isRequired,
+  age: Proptypes.number.isRequired,
+  info: Proptypes.string.isRequired,
+};
+
+Person.defaultProps = {
+  img: "https://randomuser.me/api/portraits/thumb/men/69.jpg",
+  name: "john Cho",
+  age: 69,
+  info: "there is no info available",
+};
+
+class ControlledInput extends Component {
+  state = {
+    text: "",
+  };
+
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value.toUpperCase();
+
+    this.setState({
+      text: value,
+    });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.text);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit} style={{ margin: "3rem" }}>
+        <input
+          type="text"
+          value={this.state.text}
+          onChange={this.handleChange}
+          name="firstname"
+        />
+        <button type="submit">submit</button>
+      </form>
+    );
+  }
+}
+
+class UncontrolledInput extends Component {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.firstname.value);
+    console.log(this.email.value);
+    console.log(this.text.textContent);
+  };
+  render() {
+    return (
+      <form style={{ margin: "3rem" }}>
+        <input
+          type="text"
+          name="firstname"
+          ref={(orange) => (this.firstname = orange)}
+        />
+        <input
+          type="email"
+          name="email"
+          ref={(input) => (this.email = input)}
+        />
+        <button type="submit" onClick={this.handleSubmit}>
+          submit here
+        </button>
+        <p ref={(input) => (this.text = input)}>hello world</p>
+      </form>
+    );
+  }
+}
+
+class PersonList extends Component {
+  state = {
+    people: [
+      {
+        id: 1,
+        img: "https://randomuser.me/api/portraits/thumb/men/24.jpg",
+        name: "john",
+        age: 25,
+      },
+      {
+        id: 2,
+        img: "https://randomuser.me/api/portraits/thumb/men/34.jpg",
+        name: "Brian",
+        age: 43,
+      },
+      {
+        id: 3,
+        img: "https://randomuser.me/api/portraits/thumb/men/30.jpg",
+        name: "Bill",
+        age: 42,
+        info: `i'm okay`,
+      },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        {this.state.people.map((person) => {
+          return (
+            <div>
+              <Person
+                key={person.id}
+                img={person.img}
+                name={person.name}
+                age={person.age}
+                info={person.info}
+              />
+            </div>
+          );
+        })}
+        <ControlledInput />
+        <UncontrolledInput />
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return <PersonList />;
+  }
+}
 
 export default App;
